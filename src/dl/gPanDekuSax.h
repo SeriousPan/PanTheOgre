f243de8a50cf6be28f38246198ba81d208e843cd
Zuke Artorius <andrew.straughn@gmail.com>
2026-07-03 00:01:33 -0800

    deku pipe anims fix

--- src/dl/gPanDekuSax.h
+++ src/dl/gPanDekuSax.h
@@ -4,12 +4,12 @@
 #include "ultra64.h"
 #include "global.h"
 
-extern u64 gPanDekuSax_tex_007B80_rgba16_rgba16[];
+extern u64 gPanDekuSax_tex_01D340_i8_i8[];
 extern Vtx gPanDekuSax_gPanDekuSax_mesh_layer_Opaque_vtx_cull[8];
-extern Vtx gPanDekuSax_gPanDekuSax_mesh_layer_Opaque_vtx_0[200];
+extern Vtx gPanDekuSax_gPanDekuSax_mesh_layer_Opaque_vtx_0[182];
 extern Gfx gPanDekuSax_gPanDekuSax_mesh_layer_Opaque_tri_0[];
-extern Gfx mat_gPanDekuSax_f3dlite_material_188_layerOpaque[];
-extern Gfx mat_revert_gPanDekuSax_f3dlite_material_188_layerOpaque[];
+extern Gfx mat_gPanDekuSax_f3dlite_material_264_layerOpaque[];
+extern Gfx mat_revert_gPanDekuSax_f3dlite_material_264_layerOpaque[];
 extern Gfx gPanDekuSax[];
 
 #endif
